749176e7740c603cd384a2e6aca2f1ad3b4dcac6
Victor <vramirez209@gmail.com>
2021-12-03 05:11:21 -0500

    updated disclaimer

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import {
-    currentDirectory,
-    directoryExists
-}  from './lib/files.js';
+import { currentDirectory, directoryExists } from "./lib/files.js";
+import { sayHerro } from "./lib/herro.js";
+import { askRunContext } from "./lib/inquirer.js";
 
-import {sayHerro } from './lib/herro.js';
-
-
+// Load CLI Welcome
 sayHerro();
+
+// Run user setup prompts
+const run = async () => {
+    const runContext = await askRunContext();
+    console.log(runContext);
+};
+
+run();
+
