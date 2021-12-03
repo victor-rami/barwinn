f204114dcf2f92b351f6d3983a62f46bd75e2c46
Victor <vramirez209@gmail.com>
2021-12-03 07:12:24 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-import { currentDirectory, directoryExists } from "./lib/files.js";
-import { sayHerro } from "./lib/herro.js";
-import { askRunContext } from "./lib/inquirer.js";
+import clear from "clear";
+import fs from "fs";
+import chalk from "chalk";
+import { currentDirectory, directoryExists, readConfig } from "./lib/files.js";
+import { sayDisclaimer, sayHerro } from "./lib/herro.js";
+import { getRunContext } from "./lib/inquirer.js";
+
+const CONFIG_FILE = "config.json";
 
 // Load CLI Welcome
+clear();
 sayHerro();
+sayDisclaimer();
 
 // Run user setup prompts
-const run = async () => {
-    const runContext = await askRunContext();
-    console.log(runContext);
+async function runSetup() {
+  try {
+    let runContext = await getRunContext();
+    console.log(chalk.greenBright.italic("Creating config file.."));
+    runContextString = JSON.stringify(runContext);
+    fs.writeFile("config.json", runContextString);
+    console.log(chalk.greenBright.italic.runContext);
+  } catch {
+    console.error();
+  }
 };
 
-run();
+let config = directoryExists(CONFIG_FILE)
+  ? readConfig(CONFIG_FILE)
+  : runSetup();
 
+console.log(config);
