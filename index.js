1c7f104d68331032d8551a8d52a0a9e42b62213a
Victor <vramirez209@gmail.com>
2021-12-03 15:34:09 -0500

    updated setup flow

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 import clear from "clear";
-import fs from "fs";
-import chalk from "chalk";
-import { currentDirectory, directoryExists, readConfig } from "./lib/files.js";
+import dotenv from 'dotenv';
+// import fs from "fs";
+// import chalk from "chalk";
+// import { currentDirectory, directoryExists } from "./lib/files.js";
 import { sayDisclaimer, sayHerro } from "./lib/herro.js";
-import { getRunContext } from "./lib/inquirer.js";
+import { newRunContext } from "./lib/herro.js";
 
-const CONFIG_FILE = "config.json";
 
 // Load CLI Welcome
 clear();
 sayHerro();
 sayDisclaimer();
 
-// Run user setup prompts
-async function runSetup() {
-  try {
-    const runContext = await getRunContext();
-    console.log(chalk.greenBright.italic("Creating config file.."));
-  } catch {
-    console.error();
-  }
-
-  const runContextString = JSON.stringify(runContext);
-  fs.writeFile("config.json", runContextString);
-};
-
-let config = directoryExists(CONFIG_FILE)
-  ? readConfig(CONFIG_FILE)
-  : runSetup();
+// Set bot params and env variables
+// runSetup();
+const setup = await newRunContext();
 
 
-  config;
